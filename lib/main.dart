c48fc371f5f1fba0581c62bfa11e1bb4ee0aca6b
DrRO <omyahyayahya@yahoo.com>
2025-03-31 18:47:08 +0200

    Complete Project

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class ErrorFallback extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Column(
